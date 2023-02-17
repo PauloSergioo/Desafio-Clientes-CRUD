435437536ef32c178897d964fb8cd3b721578edf
Paulo Sergio <paulosergioilha38@gmail.com>
2023-02-17 11:37:18 -0300

    PUT Client

--- src/main/java/com/challangeClients/CRUD/services/ClientService.java
+++ src/main/java/com/challangeClients/CRUD/services/ClientService.java
@@ -33,15 +33,26 @@ public class ClientService {
     @Transactional
     public ClientDTO insert(ClientDTO dto) {
             Client entity = new Client();
-            entity.setBirthDate(dto.getBirthDate());
-            entity.setChildren(dto.getChildren());
-            entity.setCpf(dto.getCpf());
-            entity.setIncome(dto.getIncome());
-            entity.setName(dto.getName());
-
+            copyDtoToEntity(dto, entity);
             entity = repository.save(entity);
-
             return new ClientDTO(entity);
-
     }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        Client entity = repository.getReferenceById(id);
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity) {
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setName(dto.getName());
+    }
+
+
 }
